8e9c8c179656f3e5d98ff9a98088b93ac4e069c2
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-06-27 06:31:26 -0700

    Add update and length checks

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple CRUD command line interface for key value stores
--- key-value.py
+++ key-value.py
@@ -1,26 +1,48 @@
-"""docstinrg"""
+"""key value store"""
+import sys
 kv_store = {}
 answering = True
 
 while (answering):
-    user_answer = input('get, insert, delete? ')
+    user_answer = input('get, insert, delete, update, or quit? ')
     if user_answer == 'insert':
-        key = input('> ')
-        value = input('> ')
+        key = input('key = ')
+        value = input('value = ')
         kv_store[key] = value
     elif user_answer == 'get':
-        print('\n')
-        for key in kv_store:
-            print(key,)
-        user_answer = input('what value to get? ')
-        try:
-            temp = kv_store[user_answer]
-            print('{{{}: {}}}'.format(user_answer, temp))
-        except Exception as e:
-            print(e)
-    else:
-        user_answer = input('What do delete? ')
-        del kv_store[user_answer]
+        keys = {k for k in kv_store}
+        if len(keys) != 0: 
+            print(keys)
+            user_answer = input('what value to get? ')
+            if user_answer not in kv_store:
+                print("key doesn't exist")
+            else:
+                temp = kv_store[user_answer]
+                print('{{{}: {}}}'.format(user_answer, temp))
+        else:
+            print('nothing to get')
+    elif user_answer == 'update':
+        if len(kv_store) != 0:
+            print(keys)
+            user_answer = input('what to update? ')
+            value = input('what to update to? ')
+            kv_store[user_answer] = value
+        else:
+            print('nothing to update')
+    elif user_answer == 'delete':
+        if len(kv_store) != 0:
+            print(kv_store)
+            user_answer = input('what to delete? ')
+            if user_answer not in kv_store:
+                print("key doesn't exist")
+            else:
+                del kv_store[user_answer]
+        else:
+            print('nothing to delete')
+    elif user_answer == 'quit':
+        print('goodbye')
+        sys.exit()
+
             
 
 
